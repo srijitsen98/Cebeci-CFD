8279acd7516ad693711ea87bf79f2cf76ffde54e
Srijit Sen <snsrijit@gmail.com>
2020-04-23 15:39:35 +0530

    Update temperaturelaplace.cpp

--- 4.6-SOR-Laplace-Equation/temperaturelaplace.cpp
+++ 4.6-SOR-Laplace-Equation/temperaturelaplace.cpp
@@ -1,21 +1,21 @@
 #include<stdio.h>
 #include<math.h>
-#define m 100
-#define n 100
+#define nx 10
+#define ny 10
 #define pi 3.14159
 int main()
 {
-    double Aw,Ae,An,As,T[m+1][n+1],Tan[m+1][n+1],Told[m+1][n+1];
+    double Aw,Ae,An,As,T[nx+1][ny+1],Tan[nx+1][ny+1],Told[nx+1][ny+1];
     double dy,dx;
     double err,w,Temp,Tempo;
     int it,i,j,loci,locj;
-    w=1.0;
-    dy=1.0/n;
-    dx=1.0/m;
-    printf("\n%f %f",dx,dy);
-    for(i=0;i<=m;i++)
+    dy=1.0/ny;
+    dx=1.0/nx;
+    w=2.0/(1+sin(pi*dx));
+    //printf("\n%f %f",dx,dy);
+    for(i=0;i<=nx;i++)
     {
-        for(j=0;j<=n;j++)
+        for(j=0;j<=ny;j++)
         {
             Tan[i][j]=exp(pi*i*dx)*sin(pi*j*dy);
         }
@@ -25,28 +25,28 @@ int main()
     An=dx*dx/(2.0*(dx*dx+dy*dy));
     As=dx*dx/(2.0*(dx*dx+dy*dy));
     j=0;
-    for(i=0;i<=m;i++)
+    for(i=0;i<=nx;i++)
     {
         T[i][j]=0;
     }
-    j=n;
-    for(i=0;i<=m;i++)
+    j=ny;
+    for(i=0;i<=ny;i++)
     {
         T[i][j]=0;
     }
     i=0;
-    for(j=1;j<n;j++)
+    for(j=1;j<ny;j++)
     {
         T[i][j]=sin(pi*j*dy);
     }
-    i=m;
-    for(j=1;j<n;j++)
+    i=nx;
+    for(j=1;j<nx;j++)
     {
         T[i][j]=exp(pi)*sin(pi*j*dy);
     }
-    for(i=1;i<m;i++)
+    for(i=1;i<nx;i++)
     {
-        for(j=1;j<n;j++)
+        for(j=1;j<ny;j++)
         {
             T[i][j]=0;
         }
@@ -57,19 +57,24 @@ int main()
     {
         err=0;
         it++;
-        for(i=1;i<m;i++)
+        for(i=1;i<nx;i++)
         {
-            for(j=1;j<n;j++)
+            for(j=1;j<ny;j++)
             {
-                Told[i][j]=T[i][j];
+                //Told[i][j]=T[i][j];
                 T[i][j]=w*(Aw*T[i-1][j]+Ae*T[i+1][j]+As*T[i][j-1]+An*T[i][j+1])+(1-w)*T[i][j];
-                if(fabs(T[i][j]-Told[i][j])>err)
+            }
+        }
+        err=0;
+        for(i=1;i<nx;i++)
+        {
+            for(j=1;j<ny;j++)
+            {
+                if(err<fabs(T[i][j]-Ae*T[i+1][j]-Aw*T[i-1][j]-As*T[i][j-1]-An*T[i][j+1]))
                 {
-                    err=fabs(T[i][j]-Told[i][j]);
                     loci=i;
                     locj=j;
-                    Temp=T[i][j];
-                    Tempo=Told[i][j];
+                    err=fabs(T[i][j]-Ae*T[i+1][j]-Aw*T[i-1][j]-As*T[i][j-1]-An*T[i][j+1]);
                 }
             }
         }
@@ -77,21 +82,21 @@ int main()
         {
             break;
         }
-        printf("\nIteration no. %d Error %f i %d j %d T %f Told %f",it,err,loci,locj,Temp,Tempo);
+        printf("\nIteration no. %d Residual %.8f",it,err);
     }
     printf("\nx\t\t y\t\t FDS\t\t AS\t\t %% Error");
-    i=20;
-    for(j=10;j<n;j+=10)
+    i=2;
+    for(j=1;j<ny;j+=1)
     {
         printf("\n%f\t %f\t %f\t %f\t %f",i*dx,j*dy,T[i][j],Tan[i][j],100.0*fabs(T[i][j]-Tan[i][j])/Tan[i][j]);
     }
-    i=50;
-    for(j=10;j<n;j+=10)
+    i=5;
+    for(j=1;j<ny;j+=1)
     {
         printf("\n%f\t %f\t %f\t %f\t %f",i*dx,j*dy,T[i][j],Tan[i][j],100.0*fabs(T[i][j]-Tan[i][j])/Tan[i][j]);
     }
-    i=90;
-    for(j=10;j<n;j+=10)
+    i=9;
+    for(j=1;j<ny;j+=1)
     {
         printf("\n%f\t %f\t %f\t %f\t %f",i*dx,j*dy,T[i][j],Tan[i][j],100.0*fabs(T[i][j]-Tan[i][j])/Tan[i][j]);
     }
